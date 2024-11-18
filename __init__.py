10e68a8113a3a98b60647a62aac49c7b3e434d06
RobeSantoro <santoro.robe@gmail.com>
2024-11-18 17:37:47 +0100

    Add doc strings

--- __init__.py
+++ __init__.py
@@ -8,10 +8,11 @@ class ListVideoPath:
 
     @classmethod
     def INPUT_TYPES(cls):
+        """ Comfy UI node input types """
         return {
             "required": {
                 "directory": ("STRING", {
-                    "default": "E:/COMFY/videos",  # Change this to your desired default directory
+                    "default": "W:\ATTRAVERSO\gammaPre\masks_portrait_3.4",  # Change this to your desired default directory
                 }),
                 "index": ("INT", {
                     "default": 0,
@@ -28,6 +29,10 @@ class ListVideoPath:
     CATEGORY = "Robe"
 
     def execute(self, directory, index, cycle):
+        """ 
+        Returns a list of video files in the specified directory and the full path of the currently selected video.
+        """
+
         videos = self.list_videos(directory)
         if not videos:
             return ([], None)  # No videos found
@@ -43,6 +48,9 @@ class ListVideoPath:
         return (videos, selected_video)
 
     def list_videos(self, directory):
+        """
+        Returns a list of video files in the specified directory.
+        """
         video_extensions = ['.mp4', '.avi', '.mov', '.mkv']
         return [f for f in os.listdir(directory) if os.path.splitext(f)[1].lower() in video_extensions]
 
@@ -54,6 +62,7 @@ class ListImagePath:
 
     @classmethod
     def INPUT_TYPES(cls):
+        """ Comfy UI node input types """
         return {
             "required": {
                 "directory": ("STRING", {
@@ -74,6 +83,9 @@ class ListImagePath:
     CATEGORY = "Robe"
 
     def execute(self, directory, index, cycle):
+        """
+        Returns a list of image files in the specified directory and the full path of the currently selected image.
+        """
         images = self.list_images(directory)
         if not images:
             return ([], None)  # No images found
@@ -89,6 +101,9 @@ class ListImagePath:
         return (images, selected_image)
 
     def list_images(self, directory):
+        """
+        Returns a list of image files in the specified directory.
+        """
         image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
         return [f for f in os.listdir(directory) if os.path.splitext(f)[1].lower() in image_extensions]
 
